f6999ee0c58be3dbfb591fcc821d79fdc37c95e7
nbuylding <nbuylding@live.ca>
2023-03-05 19:00:26 -0500

    completed daily horoscope app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Daily Horoscope
+
+This project is a daily horoscope app that provides users with their horoscope based on the current day.
+
+## Authors
+
+-   [@nbuylding](https://github.com/nbuylding)
+
+## Lessons Learned
+
+I learned how to fetch data through the aztro.sameerkumar api, as well as how to utilize an event handler for a dropdown menu.
+
+## Tech Stack
+
+-   HTML
+-   CSS
+-   JavaScript
+-   aztro.sameerkumar API
+-   Flexbox
+
+## Live Site
+
+-   [Daily Horoscope]()
---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const selectSign = document.querySelector('.sign-selection');
+const horoscopeContainer = document.querySelector('.card-container');
+
+selectSign.addEventListener('change', function () {
+	const sign = selectSign.value;
+	const URL = `https://aztro.sameerkumar.website/?sign=${sign}&day=today`;
+	fetch(URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			horoscopeContainer.innerHTML = `<h2>${data.date_range}</h2>
+            <p>${data.description}</p>
+            <ul>
+            <li><strong>Lucky Number : </strong>${data.lucky_number}</li>
+            <li><strong>Lucky Colour : </strong>${data.color}</li>
+            <li><strong>Mood : </strong>${data.mood}</li>
+            <li><strong>Compatibility :</strong> ${data.compatibility}</li>
+            </ul>
+            `;
+		});
+});
